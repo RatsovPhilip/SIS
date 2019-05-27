02b59b470d9bac191cc0dbb801c113b4ef1ef1e8
RatsovPhilip <ratsovphilip@gmail.com>
2019-05-27 21:35:56 +0300

    Added cookies and session classes

---
+++ src/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,59 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Cookies.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Cookies
+{
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private Dictionary<string, HttpCookie> httpCookies;
+
+        public HttpCookieCollection()
+        {
+            this.httpCookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void AddCookie(HttpCookie cookie)
+        {
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+            this.httpCookies.Add(cookie.Key, cookie);
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmty(key, nameof(key));
+
+            return this.httpCookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmty(key, nameof(key));
+
+            return this.httpCookies[key];
+        }
+
+        public IEnumerator<HttpCookie> GetEnumerator()
+        {
+            return this.httpCookies.Values.GetEnumerator();
+        }
+
+        public bool HasCookie()
+        {
+            return this.httpCookies.Count != 0;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(";", this.httpCookies.Values);
+        }
+    }
+}
---
+++ src/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,46 @@
+﻿using SIS.HTTP.Common;
+using SIS.HTTP.Sessions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Sessions
+{
+    public class HttpSession : IHttpSession
+    {
+        private Dictionary<string, object> parameters;
+        public string Id { get; }
+
+        public HttpSession(string id)
+        {
+            this.Id = id;
+            this.parameters = new Dictionary<string, object>();
+        }
+
+        public void AddParameter(string name, object parameter)
+        {
+            CoreValidator.ThrowIfNullOrEmty(name, nameof(name));
+            CoreValidator.ThrowIfNull(parameter, nameof(parameter));
+
+            this.parameters.Add(name, parameter);
+        }
+
+        public void ClearParameter()
+        {
+            this.parameters.Clear();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            CoreValidator.ThrowIfNullOrEmty(name, nameof(name));
+            return this.parameters.ContainsKey(name);
+        }
+
+        public object GetParameter(string name)
+        {
+            CoreValidator.ThrowIfNullOrEmty(name, nameof(name));
+
+            return this.parameters[name];
+        }
+    }
+}
